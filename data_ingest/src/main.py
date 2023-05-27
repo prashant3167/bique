db5793a939338380d7dc6401bb7ce7312edc12b3
Your Name <you@example.com>
2023-05-28 01:50:19 +0200

    new compose for new sink connector

--- data_ingest/src/main.py
+++ data_ingest/src/main.py
@@ -21,6 +21,7 @@ data_schema = StructType(fields=[
        )
     )
 ])
+schema = StructType().add("before", StringType()).add("after", StringType()).add("source", StringType())
 
 # spark = SparkSession.builder \
 #           .master("local[*]") \
@@ -80,10 +81,37 @@ df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", kafka_bo
 #     .select(json.loads("decoded_value").alias("data")) \
 #     .select("data.*")
 
+def extract_schema(json_data):
+    schema_json = json_data['schema']
+    fields = schema_json['fields']
+    schema = StructType([
+        StructField(field['field'], field['type'], field['optional'])
+        for field in fields
+    ])
+    return schema
+
+
 decode_binary = udf(lambda value: value.decode('utf-8'), StringType())
 
+decode_binary = udf(lambda value: json.loads(value.decode('utf-8')), StringType())
+# decode_binary = udf(lambda value: json.loads(value.decode('utf-8')), MapType(StringType(), StringType()))
+
+
+df = df.select(from_json(df.value.cast("string"), extract_schema(df.value)).alias("data"))
+df = df.select("data.payload.after", "data.payload.source", "data.payload.op", "data.payload.ts_ms")
 # Apply the UDF on the 'value' column
-df = df.select(decode_binary(col("value")).alias("decoded_value")).select(from_json(col("decoded_value"), "struct<username:string,date:string>").alias("data")).select("data.*")
+# df = df.select(decode_binary(col("value")).alias("decoded_value")).select(from_json(col("decoded_value"), "struct<username:string,date:string>").alias("data")).select("data.*")
+# df = df.withColumn("test",decode_binary(col("value"))).select(explode("test").alias("key", "value"))
+# df.printSchema()
+# df = df.withColumn("data", from_json(df.value.cast("string"),schema))
+# df = df.select(from_json(df.value.cast("string"), schema).alias("data"))
+# df = df.select("data.before", "data.after", "data.source")
+
+# df = df.selectExpr("explode(data) as exploded_data")
+
+# Select the flattened columns
+# df = df.select("exploded_data.*")
+# df.printSchema()
 # df = df.selectExpr("CAST(decoded_value AS STRING)").select(from_json(col("decoded_value"), "struct<username:string,date:string,transactions:array<struct<id:int,category:string,datetime:string,amount:double>>>").alias("data")).select("data.*")
 # df = df.selectExpr("CAST(value AS STRING)").select(from_json(col("value"), "struct<username:string,date:string,transactions:array<struct<id:int,category:string,datetime:string,amount:double>>>").alias("data")).select("data.*")
 # Continue with the remaining transformations
@@ -100,7 +128,7 @@ import time
     # .trigger(processingTim÷e="10 seconds") \
 output_df = df.writeStream \
     .outputMode("append") \
-    .format("parquet") \
+    .format("csv") \
     .option("checkpointLocation", "/Users/Licious/project/bique/checkpoint"+ "/" + str(int(time.time()))) \
     .trigger(processingTime="60 seconds") \
     .start(output_path + "/" + str(int(time.time())))
