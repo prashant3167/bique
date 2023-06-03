32cab1207e584e210978d87bb59126b313c63df6
risg99 <rishikagupta1999@gmail.com>
2023-06-03 18:01:42 +0200

    sdm initial commit

---
+++ graph/load_neo4j_data.py
@@ -0,0 +1,43 @@
+from session_helper import create_session, clean_session
+
+session = create_session()
+session = clean_session(session)
+
+def load_node_users(session):
+    session.run(
+        """LOAD CSV WITH HEADERS FROM 'file:///users.csv' AS line
+            CREATE (:Users {
+                id: line.id,
+                name: line.name
+        })"""
+    )
+
+
+def load_node_advisors(session):
+    session.run(
+        """LOAD CSV WITH HEADERS FROM 'file:///final_advisors.csv' AS line
+            CREATE (:Advisors {
+                id: line.id,
+                name: line.name,
+                jobcount: line.jobcount,
+                charge: line.charge
+        })"""
+    )
+
+def load_relation_users_advisors(session):
+    session.run(
+            """LOAD CSV WITH HEADERS FROM 'file:///mappings.csv' AS line
+                MATCH (user:Users {id: line.user_id})
+                WITH user, line
+                MATCH (advisor:Advisors {id: line.advisor_id})
+                CREATE (user) - [r:consulted] -> (advisor)
+                SET r.rating = toInteger(line.rating),
+                    r.date = date(line.date)"""
+    )
+
+print('Creating and loading the nodes and relations into the database...')
+session.execute_write(load_node_users)
+session.execute_write(load_node_advisors)
+session.execute_write(load_relation_users_advisors)
+
+session.close()
---
+++ graph/session_helper.py
@@ -0,0 +1,25 @@
+from neo4j import GraphDatabase
+
+def delete_and_detach_all_nodes(session):
+    session.run(
+        "MATCH (n) DETACH DELETE n"
+    )
+
+def create_session():
+    username = 'neo4j'
+    password = 'bique123'
+
+    print('Creating a connection with neo4j...')
+    driver = GraphDatabase.driver("neo4j://10.4.41.51:7687", auth=(username, password))
+
+    session = driver.session()
+    print('Session Initiated....')
+
+    return session
+
+def clean_session(session):
+    
+    print('Deleting and detaching all the previous nodes in the database.')
+    session.execute_write(delete_and_detach_all_nodes)
+
+    return session
