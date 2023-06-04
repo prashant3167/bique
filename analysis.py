075619a077f04c0166e18d08e66d18c4df727e3f
risg99 <rishikagupta1999@gmail.com>
2023-06-04 19:12:33 +0200

    prediction pipeline code modified

--- analysis.py
+++ analysis.py
@@ -2,43 +2,35 @@ import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, sum, count, explode, year, month, to_date
 from pyspark.sql.types import DoubleType
-
+from hdfs.ext.kerberos import KerberosClient
 import numpy as np
 import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
-import plotly.io as pio
-from plotly.subplots import make_subplots
-import seaborn as sns
-import matplotlib.pyplot as plt
 import warnings
-from scipy.stats import boxcox
-from statsmodels.api import qqplot
 from statsmodels.tsa.stattools import adfuller
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from statsmodels.tsa.arima.model import ARIMA
-from statsmodels.tsa.statespace.sarimax import SARIMAX
-from pmdarima.arima import auto_arima
-from pmdarima.utils import diff_inv
 from sklearn.metrics import mean_squared_error
 import datetime
-from datetime import datetime
+from datetime import date, datetime
+from dateutil.relativedelta import relativedelta
 
 warnings.filterwarnings("ignore")
-sns.set(rc = {'figure.figsize': (5, 3)})
-plt.style.use("fivethirtyeight")
-rand_val = 765
 
 spark = SparkSession.builder.appName("Read from HDFS").getOrCreate()
 
-hdfs_path = "hdfs://10.4.41.51:27000/user/bdm/formatted_data/bique.transactions/fullDocument_source="
-
-users = ["ES02JHOP23942038749660", "ES03GWNO51408147941807", "ES06WFGU72983192679063"]
+hdfs_client = KerberosClient('http://10.4.41.51:9870')
+users = hdfs_client.list('/user/bdm/formatted_data/bique.transactions')
 
 predictions = {}
 
 for user in users:
-    data = spark.read.parquet(hdfs_path + user)
+    file_path = 'hdfs://10.4.41.51:27000/user/bdm/formatted_data/bique.transactions/'
+    file_name = user
+    if file_name != '_SUCCESS':
+        file_path += file_name
+    else:
+        continue
+    # print(file_name)
+    data = spark.read.parquet(file_path)
     
     # data transformations
     data = data.withColumn("fullDocument_transactionAmount", col("fullDocument_transactionAmount_amount").cast(DoubleType()))
@@ -50,20 +42,45 @@ for user in users:
     df['date'] = pd.to_datetime(df['date']).apply(lambda x : x.replace(day=1))
     df = df.groupby("date").sum().sort_values('date', ascending = True)
     
-    fdiff_ts = df.diff().dropna()
-    sdiff_ts = fdiff_ts.diff().dropna()
+    # creating training and validation, with a dynamic percentage split of 80%-20%, depending on the values available
+    perc_split = int(len(df) * 0.8)
     
-    model = ARIMA(sdiff_ts, order = (0, 0, 1))
+    # train and validation here
+    train_df = df.iloc[:perc_split]
+    test_df = df.iloc[perc_split:]
     
+    test_df.loc[test_df.index[-1] + relativedelta(months=+1)] = 0
+    # print(train_df.shape, test_df.shape)
+    
+    result = adfuller(df['amount'])
+    # print('ADF Statistic: {}'.format(result[0]))
+    # print('p-value: {}'.format(result[1]))
+    
+    model = ARIMA(train_df, order = (2,3,3))
     model_fit = model.fit()
-    pred = model_fit.forecast()
-    pred = pred.to_frame().rename(columns = {0: 'forecast'}).reset_index().rename(columns = {'index':'forecast_date'})
-    pred['forecast_cumsum'] = pred['forecast'].cumsum()
-    pred['first'] = pred['forecast_cumsum'] + fdiff_ts.reset_index()[-1:]['amount'].item()
-    pred['first_cumsum'] = pred['first'].cumsum()
-    pred['second'] = pred['first_cumsum'] + df.reset_index()[-1:]['amount'].item()
-    print('Prediction for user: ' + user + ' for the month-year of ' + pred.forecast_date.item().strftime('%B %Y') + ' to be: ' + str(pred.second.item()))
-    predictions[user] = pred.second.item()
+    predictions = model_fit.predict(start = len(train_df), end = len(train_df) + len(test_df) - 1)
+    pred_df = predictions.to_frame().rename(columns = {0: 'date', 'predicted_mean': 'forecast'})
+    
+    # Calculate accuracy metrics
+    actual_values = test_df['amount']
+    prediction_values = pred_df['forecast']
+    mae = np.mean(np.abs(prediction_values - actual_values))
+    mse = np.mean((prediction_values - actual_values) ** 2)
+    rmse = np.sqrt(mse)
+
+    # print(f"Mean Absolute Error (MAE): {mae}")
+    # print(f"Mean Squared Error (MSE): {mse}")
+    # print(f"Root Mean Squared Error (RMSE): {rmse}")
+    
+    predictions[user[20:]] = pred_df['forecast'][-1]
 
 print('After processing all users:')
-print(predictions)
+# print(predictions)
+
+output_df = pd.DataFrame(predictions.items(), columns=['user', 'amount'])
+
+hdfs_path = "hdfs://10.4.41.51:27000/user/bdm/exploited_zone/aggregations/nextPrediction"
+
+output_df.write.format("csv").mode("overwrite").save(hdfs_path, user = "bdm")
+
+spark.stop()
